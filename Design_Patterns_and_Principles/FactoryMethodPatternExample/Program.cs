82dc1ae297145326b67607b62d7eca7c4e487dce
Ayush Kumar <ayushagrawal.5288@gmail.com>
2025-06-19 21:17:52 +0530

    Changed To C#

---
+++ Design_Patterns_and_Principles/FactoryMethodPatternExample/Program.cs
@@ -0,0 +1,100 @@
+﻿using System;
+
+interface IDocument
+{
+    void Open();
+    void Save();
+}
+
+class WordDocument:IDocument
+{
+    public void Open()
+    {
+        Console.WriteLine("Opening Word document");
+    }
+
+    public void Save()
+    {
+        Console.WriteLine("Saving Word document");
+    }
+}
+
+class PdfDocument:IDocument
+{
+    public void Open()
+    {
+        Console.WriteLine("Opening PDF document");
+    }
+
+    public void Save()
+    {
+        Console.WriteLine("Saving PDF document");
+    }
+}
+
+class ExcelDocument:IDocument
+{
+    public void Open()
+    {
+        Console.WriteLine("Opening Excel document");
+    }
+
+    public void Save()
+    {
+        Console.WriteLine("Saving Excel document");
+    }
+}
+
+abstract class DocumentFactory
+{
+    public abstract IDocument CreateDocument();
+}
+
+class WordFactory:DocumentFactory
+{
+    public override IDocument CreateDocument()
+    {
+        return new WordDocument();
+    }
+}
+
+class PdfFactory:DocumentFactory
+{
+    public override IDocument CreateDocument()
+    {
+        return new PdfDocument();
+    }
+}
+
+class ExcelFactory:DocumentFactory
+{
+    public override IDocument CreateDocument()
+    {
+        return new ExcelDocument();
+    }
+}
+
+class DocumentFactoryMethod
+{
+    static void Main(string[] args)
+    {
+        DocumentFactory wordFactory=new WordFactory();
+        IDocument wordDoc=wordFactory.CreateDocument();
+        wordDoc.Open();
+        wordDoc.Save();
+        Console.WriteLine();
+
+        DocumentFactory pdfFactory=new PdfFactory();
+        IDocument pdfDoc=pdfFactory.CreateDocument();
+        pdfDoc.Open();
+        pdfDoc.Save();
+        Console.WriteLine();
+
+        DocumentFactory excelFactory=new ExcelFactory();
+        IDocument excelDoc=excelFactory.CreateDocument();
+        excelDoc.Open();
+        excelDoc.Save();
+        Console.WriteLine();
+        Console.ReadLine();
+    }
+}
---
+++ Design_Patterns_and_Principles/SingletonPatternExample/Program.cs
@@ -0,0 +1,37 @@
+﻿class Logger
+{
+    private static Logger instance;
+
+    private Logger() {}
+
+    public static Logger GetInstance()
+    {
+        if(instance==null)
+        {
+            instance=new Logger();
+        }
+        return instance;
+    }
+
+    public void Print(string message)
+    {
+        Console.WriteLine(message);
+    }
+}
+
+class Logging
+{
+    static void Main(string[] args)
+    {
+        Logger logger1=Logger.GetInstance();
+        Logger logger2=Logger.GetInstance();
+
+        logger1.Print("Tracking from logger1");
+        logger2.Print("Tracking from logger2");
+
+        Console.WriteLine("Both Instance same or not:- "+(logger1==logger2));
+        Console.ReadLine();
+    }
+} 
+
+
