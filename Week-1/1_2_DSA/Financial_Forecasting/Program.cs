ecd6330cd68d7eb30905e312f2a68f254cdedd52
Ayush Kumar <ayushagrawal.5288@gmail.com>
2025-06-21 16:47:29 +0530

    week_1_2_2_Financial_Forecasting

--- Week-1/1_2_DSA/Financial_Forecasting/Program.cs
+++ Week-1/1_2_DSA/Financial_Forecasting/Program.cs
@@ -1,2 +1,44 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+public class Forecast
+{
+    public static double Future(double p,double r,int y)
+    {
+        if(y==0)
+        {
+            return p;
+        }
+        return Future(p,r,y-1)*(1+r);
+    }
+
+    public static void Print(double p,double r,int y)
+    {
+        Console.WriteLine("\nprediction:-");
+        for(int i=0;i<=y;i++)
+        {
+            double val=Future(p,r,i);
+            Console.WriteLine($"year {i}: ₹ {val:F2}");
+        }
+    }
+
+    public static void Main()
+    {
+        try
+        {
+            Console.Write("Initial amount: ");
+            double p=Convert.ToDouble(Console.ReadLine());
+            Console.Write("Growth rate(%): ");
+            double r=Convert.ToDouble(Console.ReadLine())/100;
+            Console.Write("Years: ");
+            int y=Convert.ToInt32(Console.ReadLine());
+
+            double final=Future(p,r,y);
+            Console.WriteLine($"\nFinal Value: ₹ {final:F2}");
+            Print(p,r,y);
+        }
+        catch
+        {
+            Console.WriteLine("Invalid input");
+        }
+    }
+}
