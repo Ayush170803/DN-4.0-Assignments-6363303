ac4170a4fd9fa3b941f37804a688fe41f299b980
Ayush Kumar <ayushagrawal.5288@gmail.com>
2025-06-28 22:02:56 +0530

    Added week_2_2_1  Nunit Testing

---
+++ Week-2/2_2_MOQ_NUNIT/NUnit-Handson/CalcLibrary.Tests/UnitTest1.cs
@@ -0,0 +1,33 @@
+﻿namespace CalcLibrary.Tests
+{
+    [TestFixture]
+    public class CalculatorTests
+    {
+        private SimpleCalculator calc;
+
+        [SetUp]
+        public void Setup()
+        {
+            calc=new SimpleCalculator();
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            calc.AllClear();
+        }
+
+        [Test]
+        [TestCase(5,3,8)]
+        [TestCase(10,-5,5)]
+        [TestCase(2,3,5)]
+        [TestCase(-1,-2,-3)]
+        [TestCase(3,1,4)]
+        [TestCase(991,9,1000)]
+        public void Test_Addition(double a,double b,double expected)
+        {
+            double result=calc.Addition(a,b);
+            Assert.That(result,Is.EqualTo(expected));
+        }
+    }
+}
